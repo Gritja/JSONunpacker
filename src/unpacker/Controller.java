dee46ea313c9fe851bbf0b2b22475e68f7c13013
Gritja <jonatan.arlock@gritacademy.se>
2020-12-05 18:32:38 +0100

    v09

--- src/unpacker/Controller.java
+++ src/unpacker/Controller.java
@@ -38,7 +38,9 @@ public class Controller {
 	public void Login(ActionEvent event) throws Exception {
 		
 		loginBtn.setOnAction((ActionEvent e) -> {
-		
+			
+			if (Users.loginUser(txtUserName.getText(), txtPassword.getText()))
+			{
 				lblStatus.setStyle("-fx-text-fill: #008000");
 				lblStatus.setText("Login Success");
 				Stage primaryStage = new Stage();
@@ -65,6 +67,7 @@ public class Controller {
 			    primaryStage.setScene(scene);
 
 			    primaryStage.show();
+			}
 			    
 			    //System.out.println(Reader.getJson());
 			    
@@ -85,6 +88,13 @@ public class Controller {
 		registerBtn.setOnAction((ActionEvent e) -> {
 			if (txtUserName.getText().length() > 3) {
 				if (txtPassword.getText().length() > 3) {
+					if (txtUserName.getText().matches(
+							"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+							Users.addUser(txtUserName.getText(), txtPassword.getText());
+					} else {
+						//Invalid email prompt!
+						System.out.println("Invalid email adress.");
+					}
 					//gå igenom listan, lägg till användaren, sedan add.arraylist(users?) > uppdatera, write to file? borde funka skitbra!
 				}
 			}
--- src/unpacker/Users.java
+++ src/unpacker/Users.java
@@ -1,33 +1,89 @@
 package unpacker;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class Users {
 	
 	//hanterar inlogg/användare-lösen
 
-    private String userName = null;
-    private String passWord = null;
+    //private String userName = null;
+    //private String passWord = null;
+	
+	//gör kolla-igenom-lista metod för att korta av repetitioner(?) med pw check så kan en förmodligen skicka in userNo fr positionen i listan.
+	//private ArrayList<String> userList;
 
-    public Users() {
+    public static boolean loginUser(String userName, String passWord) {
+		boolean allGood = false;
+    	try {
+    		ArrayList<String> userList = new ArrayList<>(Files.readAllLines(Paths.get("C:\\Users\\astro\\eclipse-workspace\\JSONunpacker\\src\\unpacker\\userpw.txt")));
+
+    		
+    		for (String fromArray : userList) {
+    			// ^^ går igenom ALLA strängar i listan och gör samma koll - behöver uppenbarligen sätta ett stopp när den hitta användaren och sen göra kollen.
+        		if (fromArray.contains(userName)) {
+        			//log
+        			if (fromArray.contains(passWord)) {
+        				//log
+        				System.out.print("User and pass accepted!");
+        				allGood = true;
+        			} else {
+        				//javafx prompt!
+        				System.out.println("Invalid password!");
+        			}
+        		} else {
+        			// javafx prompt!
+        			System.out.println(fromArray);
+        			//log
+        			for (int i = 0; i < userList.size(); i++) {
+        				System.out.println(userList.get(i));
+        			}
+        			
+        			System.out.println("Invalid email!");
+        		}
+        	}
+    	} catch (IOException e) {
+    		
+    	}
+    	//log
+    	System.out.print("Ran!");
+		return allGood;
+
     }
 
-    public Users(String firstName, String passWord) {
-        this.userName = userName;
-        this.passWord = passWord;
-    }
-
-    public String getuserName() {
-        return userName;
-    }
-
-    public void setFirstName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getpassWord() {
-        return passWord;
-    }
-
-    public void setLastName(String passWord) {
-        this.passWord = passWord;
-    }
+	public static void addUser(String userName, String passWord) {
+		try {
+			ArrayList<String> userList = new ArrayList<>(Files.readAllLines(Paths.get("C:\\Users\\astro\\eclipse-workspace\\JSONunpacker\\src\\unpacker\\userpw.txt")));
+			int userNo = userList.size();
+			boolean userExists = false;
+			
+			//check substrings in arraylist for user name. could be a separate method that returns true... but I figure this is fine too.
+			for (String fromArray : userList) {
+				if (fromArray.contains(userName)) {
+					userExists = true;
+				}
+			}
+			if (!userExists) {
+			userList.add(++userNo + ": " + userName + ", " + passWord);
+			Path updateList = Paths.get("C:\\Users\\astro\\eclipse-workspace\\JSONunpacker\\src\\unpacker\\userpw.txt");
+			Files.write(updateList,userList,Charset.defaultCharset());
+			
+			} else {
+				//add fx prompt "user already exists!"
+				System.out.println("User already exists!");
+			}
+			//log
+			for (int i = 0; i < userList.size(); i++) {
+				System.out.println(userList.get(i));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
